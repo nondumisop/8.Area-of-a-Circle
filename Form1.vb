31549383ffb3e349c8f9fcd03ec938b1835afa67
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-18 06:44:51 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim A As Double
+        Const pi As Double = 3.14
+        Dim r As Double
+
+        r = Val(TextBox1.Text)
+        A = pi * r ^ 2
+
+        TextBox2.Text = A
+
+    End Sub
+End Class
